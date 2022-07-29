ba6c800c75e45c15f1042bad94a7953365c46502
bishabosi <109702458+bishabosi@users.noreply.github.com>
2022-07-30 00:37:09 +0200

    vscode

--- public/watch.js
+++ public/watch.js
@@ -19,15 +19,13 @@ socket.on("offer", (id, description) => {
       .then(() => {
         console.log("answer")
         socket.emit("answer", id, peerConnection.localDescription);
-        peerConnection.onaddstream = event => {
-          video.srcObject = event.stream;
-          video.muted = false;
-        };
+        
       })
     })
-      
-    
-
+    peerConnection.onaddstream = event => {
+      video.srcObject = event.stream;
+      video.muted = false;
+    }; 
     
   peerConnection.onicecandidate = event => {
     if (event.candidate) {
