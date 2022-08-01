beae4bd5ea11dadd8ce66c03dfbffe202dc1fa73
bishabosi <109702458+bishabosi@users.noreply.github.com>
2022-08-01 03:26:41 +0200

    vscode

--- public/broadcast.js
+++ public/broadcast.js
@@ -66,34 +66,13 @@ window.onunload = window.onbeforeunload = () => {
 
 // Get camera and microphone
 const videoElement = document.querySelector("video");
-const audioSelect = document.querySelector("select#audioSource");
-const videoSelect = document.querySelector("select#videoSource");
+//const audioSelect = document.querySelector("select#audioSource");
+//const videoSelect = document.querySelector("select#videoSource");
 
-audioSelect.onchange = getStream;
-videoSelect.onchange = getStream;
+//audioSelect.onchange = getStream;
+//videoSelect.onchange = getStream;
 
 getStream()
-  .then(getDevices)
-  .then(gotDevices);
-
-function getDevices() {
-  return navigator.mediaDevices.enumerateDevices();
-}
-
-function gotDevices(deviceInfos) {
-  window.deviceInfos = deviceInfos;
-  for (const deviceInfo of deviceInfos) {
-    const option = document.createElement("option");
-    option.value = deviceInfo.deviceId;
-    if (deviceInfo.kind === "audioinput") {
-      option.text = deviceInfo.label || `Microphone ${audioSelect.length + 1}`;
-      audioSelect.appendChild(option);
-    } else if (deviceInfo.kind === "videoinput") {
-      option.text = deviceInfo.label || `Camera ${videoSelect.length + 1}`;
-      videoSelect.appendChild(option);
-    }
-  }
-}
 
 function getStream() {
   if (window.stream) {
@@ -104,8 +83,8 @@ function getStream() {
   const audioSource = audioSelect.value;
   const videoSource = videoSelect.value;
   const constraints = {
-    audio: { deviceId: audioSource ? { exact: audioSource } : undefined },
-    video: { deviceId: videoSource ? { exact: videoSource } : undefined }
+    audio: true,
+    video: true
   };
   return navigator.mediaDevices
     .getDisplayMedia(constraints)
@@ -115,12 +94,7 @@ function getStream() {
 
 function gotStream(stream) {
   window.stream = stream;
-  audioSelect.selectedIndex = [...audioSelect.options].findIndex(
-    option => option.text === stream.getAudioTracks()[0].label
-  );
-  videoSelect.selectedIndex = [...videoSelect.options].findIndex(
-    option => option.text === stream.getVideoTracks()[0].label
-  );
+  
   videoElement.srcObject = stream;
   socket.emit("broadcaster");
 }
